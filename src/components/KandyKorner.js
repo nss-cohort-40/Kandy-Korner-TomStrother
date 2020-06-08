efa83e8f3dfb316bdaf5870eaaeab882cc23d90a
Tom Strother <tom_strother@yahoo.com>
2020-06-08 11:30:27 -0500

    initial commit with file structure

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./KandyKorner.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Kandy Korner</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
